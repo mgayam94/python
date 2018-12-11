343143c3784da4bc47a654fe727c00ad29888443
mgayam94 <31038049+mgayam94@users.noreply.github.com>
2018-12-11 14:50:51 +0530

    Update app3-website-blocker.py

--- app3-website-blocker.py
+++ app3-website-blocker.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime as dt
 
 hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
-hosts_path="/etc/hosts"
+hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
 

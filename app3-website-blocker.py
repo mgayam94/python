99bfb525c42740b777f0725bec0e30497411e8f8
mgayam94 <31038049+mgayam94@users.noreply.github.com>
2018-12-11 14:53:37 +0530

    Update app3-website-blocker.py

--- app3-website-blocker.py
+++ app3-website-blocker.py
@@ -2,13 +2,14 @@ import time
 from datetime import datetime as dt
 
 hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
-hosts_path="C:\Windows\System32\drivers\etc\hosts"
+hosts_path="C:\Windows\System32\drivers\etc\hosts" #file path of hosts.
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
+website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"] #these are the websites to be blocked.
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16): #condition to check the time between 8.00am to 04.00pm
         print("Working hours...")
+ #code below helps to open the file and perform the actions.
         with open(hosts_path,'r+') as file:
             content=file.read()
             for website in website_list:
@@ -25,4 +26,4 @@ while True:
                     file.write(line)
             file.truncate()
         print("Fun hours...")
-    time.sleep(5)
+    time.sleep(5) #used to run the code for every 5sec.
